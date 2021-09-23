2243d522e8e574c9ea17c135433b4daf864477d6
fjorwb <fjoliveri.wb@gmail.com>
2021-09-23 18:51:13 -0500

    HTML, CSS.header v1

--- js/Main.js
+++ js/Main.js
@@ -1,4 +1,4 @@
-const context = {
+const context1 = {
   projects: [
     {
       image: "img/img01.jpg",
@@ -9,9 +9,77 @@ const context = {
   ],
 };
 
-const templateElement = document.getElementById("templatePF");
-const templateSource = templateElement.innerHTML;
-const template = Handlebars.compile(templateSource);
+const templateElement1 = document.getElementById("templateWK");
+const templateSource1 = templateElement1.innerHTML;
+const template1 = Handlebars.compile(templateSource1);
 
-compiledHtml = template(context);
-document.getElementById("work").innerHTML = compiledHtml;
+compiledHtml1 = template1(context1);
+document.getElementById("work").innerHTML = compiledHtml1;
+
+const context2 = {
+  skills: [
+    {
+      tools: "fab fa-html5",
+      show: true,
+    },
+    {
+      tools: "fab fa-css3-alt",
+      show: true,
+    },
+    {
+      tools: "fab fa-js",
+      show: true,
+    },
+    {
+      tools: "fab fa-sass",
+      show: true,
+    },
+    {
+      tools: "fab fa-bootstrap",
+      show: true,
+    },
+    {
+      tools: "fab fa-node-js",
+      show: true,
+    },
+    {
+      tools: "fab fa-react",
+      show: true,
+    },
+    {
+      tools: "fab fa-github",
+      show: true,
+    },
+    {
+      tools: "fab fa-git",
+      show: true,
+    },
+    {
+      tools: "fab fa-python",
+      show: true,
+    },
+    {
+      tools: "fab fa-firefox",
+      show: true,
+    },
+    {
+      tools: "fab fa-file-excel",
+      show: true,
+    },
+    {
+      tools: "fab fa-wordpress",
+      show: true,
+    },
+    {
+      tools: "fab fa-firefox",
+      show: true,
+    },
+  ],
+};
+
+const templateElement2 = document.getElementById("templateSK");
+const templateSource2 = templateElement2.innerHTML;
+const template2 = Handlebars.compile(templateSource2);
+
+compiledHtml = template2(context2);
+document.getElementById("skill").innerHTML = compiledHtml;
